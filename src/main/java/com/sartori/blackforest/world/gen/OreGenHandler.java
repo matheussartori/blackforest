6b348ba8860491b473a29c2e0cafcc99c56b61cb
Matheus Sartori <ms.sartori@outlook.com.br>
2020-10-30 21:34:27 -0300

    Update OreGenHandler.java

--- src/main/java/com/sartori/blackforest/world/gen/OreGenHandler.java
+++ src/main/java/com/sartori/blackforest/world/gen/OreGenHandler.java
@@ -1,16 +1,19 @@
 package com.sartori.blackforest.world.gen;
 
 import com.sartori.blackforest.BlackForest;
+import com.sartori.blackforest.registry.bundles.PolarisBundle;
 import net.minecraft.block.BlockState;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
 import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.registries.ForgeRegistries;
-
-import static net.minecraft.world.biome.Biome.Category.*;
 
 @Mod.EventBusSubscriber(modid = BlackForest.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class OreGenHandler {
@@ -27,12 +30,16 @@ public class OreGenHandler {
                     break;
                 // WORLD GENERATION
                 default:
+                    genOre(biome, 15, 8, 5, 10, OreFeatureConfig.FillerBlockType.NATURAL_STONE, PolarisBundle.POLARIS_ORE.get().getDefaultState(), 6);
             }
 
         }
     }
 
     private static void genOre(Biome biome, int count, int bottomOffset, int topOffset, int max, OreFeatureConfig.FillerBlockType filler, BlockState defaultBlockState, int size) {
-
+        CountRangeConfig range = new CountRangeConfig(count, bottomOffset, topOffset, max);
+        OreFeatureConfig feature = new OreFeatureConfig(filler, defaultBlockState, size);
+        ConfiguredPlacement<CountRangeConfig> config = Placement.COUNT_RANGE.configure(range);
+        biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(feature).withPlacement(config));
     }
 }
